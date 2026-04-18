7a9a7fdfd3c57a788e2683c57bf1bde347cbcf87
pvpx3055 <gaoussoucoulibaly847@gmail.com>
2026-04-18 09:20:14 +0000

    Mise en place d’un modèle de robustesse du script adapté à SQL SERVER

--- BDD.sql
+++ BDD.sql
@@ -1,35 +1,48 @@
-CREATE DATABASE gestion_université 
-
-CREATE enseignant (
+IF NOT EXISTS (Select * from sys.database where name = "database_univ")
+ CREATE DATABASE database_univ
+GO
+USE database_univ
+IF NOT EXISTS (select * from sys.tables where name = "enseignant")
+CREATE TABLE enseignant (
   id_enseignant PRIMARY KEY,
-  nom VARCHAR(20),
-  prenom VARCHAR(20),
-  specialité varchar(20)
+  nom VARCHAR(20)NOT NULL,
+  prenom VARCHAR(20)NOT NULL,
+  specialité varchar(20) NOT NULL,
+  email VARCHAR(30) UNIQUE
   );
-CREATE filiere (
+GO
+IF NOT EXISTS (select * from sys.tables where ="filiere")
+CREATE TABLE filiere (
   id_filiere PRIMARY KEY,
-  libelle VARCHAR(20)
+  libelle VARCHAR(20) NOT NULL
   );
-CREATE niveau (
+GO
+IF NOT EXISTS (select * from sys.tables where name = "niveau")
+CREATE TABLE  niveau (
   id_niveau PRIMARY KEY ,
-  libelle VARCHAR(20)
+  libelle VARCHAR(20) NOT NULL
   );
-CREATE etudiant (
+GO
+IF NOT EXISTS (select * from sys.table where name = "étudiant"
+CREATE TABLE etudiant (
   id_etudiant PRIMARY KEY,
-  nom VARCHAR (20),
-  prenom VARCHAR(20),
+  nom VARCHAR (20) NOT NULL,
+  prenom VARCHAR(20) NOT NULL,
   filiere INT,
   niveau INT
   );
+GO
+IF NOT EXISTS ( select * from sys.tables where name ="cours")
 CREATE TABLE cours (
   id_cours PRIMARY KEY ,
-  nom_cour VARCHAR(20),
-  type_eva VARCHAR(20),
+  nom_cour VARCHAR(20) NOT NULL,
   professeur INT,
   coefficient INT,
   filiere INT,
   niveau INT 
 );
+GO
+IF NOT EXISTS (select * from sys.tables where name = "note")
 CREATE TABLE note (
   id_note PRIMARY KEY,
   id_etudiant INT,
@@ -37,3 +50,4 @@ CREATE TABLE note (
   date DATE,
   type_eva 
   );
+GO
